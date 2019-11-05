96ffe9ee6d9262d27643f6d968faeccbf9857ef7
BBT Software AG <devadmin@bbtsoftware.ch>
2019-11-05 11:17:02 +0000

    AU: 1 updated - ssis-vs2019 [skip ci] https://gist.github.com/b601bc1e0835054f4df23310100983ee/36c893c1fb024042e1fd6611ee83574346ba29b1

--- automatic/ssis-vs2019/tools/ChocolateyInstall.ps1
+++ automatic/ssis-vs2019/tools/ChocolateyInstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
     fileType      = 'exe'
     silentArgs    = "/quiet /log `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
     validExitCodes= @(0)
-    url           = "https://ssis.gallerycdn.vsassets.io/extensions/ssis/sqlserverintegrationservicesprojects/3.1/1562574939108/Microsoft.DataTools.IntegrationServices.exe"
-    checksum      = "412eb7ccf46bb6ee9528921efb829184ff3232bd5fc121544b20f50abdeeef0b"
+    url           = "https://ssis.gallerycdn.vsassets.io/extensions/ssis/sqlserverintegrationservicesprojects/3.2/1572932535239/Microsoft.DataTools.IntegrationServices.exe"
+    checksum      = "8cbd84e059258729e76b886c6a1251944b4e1ec6f5a01aa207622204a0d50d1d"
     checksumType  = "sha256"
   }
   
