0d7a72fffbc77e0b4df6886b21df6aacd6a824a1
Pascal Berger <bep@bbtsoftware.ch>
2020-10-23 09:41:22 +0200

    (GH-96) Support installation  through background service

--- automatic/markdownlint-cli/tools/chocolateyInstall.ps1
+++ automatic/markdownlint-cli/tools/chocolateyInstall.ps1
@@ -2,11 +2,4 @@
 
 $version = '0.23.2'
 
-if (-Not (Get-Command "node" -errorAction SilentlyContinue)) {
-    Write-Error "MarkdownLint Command Line Interface requires Node.js to be installed. To install with Chocolatey, use either of the commands below:"
-    Write-Error "  choco install nodejs"
-    Write-Error "  choco install nodejs-lts"
-    throw "Node.js not found"
-} else {
-    npm install -g markdownlint-cli@$version
-}
+Install-NpmPackage markdownlint-cli@$version
