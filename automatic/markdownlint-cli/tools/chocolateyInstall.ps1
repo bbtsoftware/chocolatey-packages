39c82439523870a6dc60ccda67e2c176de3476f5
BBT Software AG <devadmin@bbtsoftware.ch>
2023-07-21 11:09:15 +0000

    AU: 2 updated - markdownlint-cli prtgdesktop [skip ci] https://gist.github.com/bbt-bot/b601bc1e0835054f4df23310100983ee/a206a11dab06be1c0a6bed14c5147ce0cc2afaff

--- automatic/markdownlint-cli/tools/chocolateyInstall.ps1
+++ automatic/markdownlint-cli/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿Update-SessionEnvironment
 
-$version = '0.34.0'
+$version = '0.35.0'
 
 Install-NpmPackage markdownlint-cli@$version
--- automatic/prtgdesktop/tools/chocolateyinstall.ps1
+++ automatic/prtgdesktop/tools/chocolateyinstall.ps1
@@ -6,12 +6,12 @@ $packageArgs = @{
 
   fileType      = 'exe'
 
-  url            = "https://downloads.paessler.com/prtg_desktop/23.4.0/32bit/prtg-desktop-offline-23.4.0-32bit.exe"
-  checksum       = "86bb4440485590d1cfa143765283e31b176774e4"
+  url            = "https://downloads.paessler.com/prtg_desktop/23.7.0/32bit/prtg-desktop-offline-23.7.0-32bit.exe"
+  checksum       = "c8f25807e4b7bf0fb128ef63848eafd1cda25693"
   checksumType   = "SHA1"
 
-  url64          = "https://downloads.paessler.com/prtg_desktop/23.4.0/64bit/prtg-desktop-offline-23.4.0-64bit.exe"
-  checksum64     = "f571a2e315bbcd5360892cba6ca4137dcdd60d09"
+  url64          = "https://downloads.paessler.com/prtg_desktop/23.7.0/64bit/prtg-desktop-offline-23.7.0-64bit.exe"
+  checksum64     = "d0e877117f21b1c36814324bf1469c3a60f302b0"
   checksumType64 = "SHA1"
 
   silentArgs     = '/quiet /norestart'
