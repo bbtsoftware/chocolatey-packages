2bd38c95c77ac86b6b3d28e433e255645ab6ee04
Christian Bumann <bch@bbtsoftware.ch>
2024-05-24 16:41:53 +0200

    Use Install-VisualStudioVsixExtension from chocolatey-visualstudio.extension package to install the extension
    Added reference to chocolatey-visualstudio.extension version 1.11.0

--- automatic/ssis-vs2022/tools/ChocolateyInstall.ps1
+++ automatic/ssis-vs2022/tools/ChocolateyInstall.ps1
@@ -1,14 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageArgs = @{
-    packageName   = $env:ChocolateyPackageName
-    softwareName  = 'Microsoft.DataTools.IntegrationServices*'
-    fileType      = 'exe'
-    silentArgs    = "/quiet /log `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
-    validExitCodes= @(0)
-    url           = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/SSIS/vsextensions/MicrosoftDataToolsIntegrationServices/1.4/vspackage"
-    checksum      = "bc6ccc273d7695d2fe2b42c4c0c16b3a59a3f6212807604a7f93ae5733527a49"
-    checksumType  = "sha256"
-  }
-  
-Install-ChocolateyPackage @packageArgs
+Install-VisualStudioVsixExtension `
+    -PackageName 'ssis-vs2022'
+    -VsixUrl 'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/SSIS/vsextensions/MicrosoftDataToolsIntegrationServices/1.4/vspackage'
+    -Checksum 'bc6ccc273d7695d2fe2b42c4c0c16b3a59a3f6212807604a7f93ae5733527a49'
+    -ChecksumType 'sha256'
