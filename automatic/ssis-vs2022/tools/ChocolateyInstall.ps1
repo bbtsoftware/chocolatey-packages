7f50cfff45d7db648a86def845b386ec7d91ac63
BBT Software AG <devadmin@bbtsoftware.ch>
2024-05-27 15:55:56 +0000

    AU: 1 updated - ssis-vs2022 [skip ci] https://gist.github.com/bbt-bot/b601bc1e0835054f4df23310100983ee/16b933035e9b5d6b64281652ce751fe0a82c346f

--- automatic/ssis-vs2022/tools/ChocolateyInstall.ps1
+++ automatic/ssis-vs2022/tools/ChocolateyInstall.ps1
@@ -2,6 +2,6 @@
 
 Install-VisualStudioVsixExtension `
     -PackageName 'ssis-vs2022'
-    -VsixUrl 'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/SSIS/vsextensions/MicrosoftDataToolsIntegrationServices/1.3.2/vspackage'
-    -Checksum '63F5DA2B6391E5326F76E6B0589EFE119AAD2D067D6B283D67C3E22CB0062FBE'
+    -VsixUrl 'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/SSIS/vsextensions/MicrosoftDataToolsIntegrationServices/1.4/vspackage'
+    -Checksum 'bc6ccc273d7695d2fe2b42c4c0c16b3a59a3f6212807604a7f93ae5733527a49'
     -ChecksumType 'sha256'
