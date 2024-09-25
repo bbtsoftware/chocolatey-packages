f249a7d8b892c478f38429e8404ee14d1f312dc4
BBT Software AG <devadmin@bbtsoftware.ch>
2024-09-25 10:03:56 +0000

    AU: 1 updated - prtgdesktop [skip ci] https://gist.github.com/bbt-bot/b601bc1e0835054f4df23310100983ee/4730d19e7a56f5b5979e3a256e0cfaf1ec755f8a

--- automatic/prtgdesktop/tools/chocolateyinstall.ps1
+++ automatic/prtgdesktop/tools/chocolateyinstall.ps1
@@ -6,12 +6,12 @@ $packageArgs = @{
 
   fileType      = 'exe'
 
-  url            = "https://downloads.paessler.com/prtg_desktop/24.9.0/32bit/prtg-desktop-offline-24.9.0-32bit.exe"
-  checksum       = "85967e2feca6678c30cd2c5222f978c5ed774bc0"
+  url            = "https://downloads.paessler.com/prtg_desktop/24.10.0/32bit/prtg-desktop-offline-24.10.0-32bit.exe"
+  checksum       = "7b743c259ba1cd9614dd6c1fb67b799da5c6f8a9"
   checksumType   = "SHA1"
 
-  url64          = "https://downloads.paessler.com/prtg_desktop/24.9.0/64bit/prtg-desktop-offline-24.9.0-64bit.exe"
-  checksum64     = "4d681bf7c6ccef94256c167cc3f22a308f8c3cb3"
+  url64          = "https://downloads.paessler.com/prtg_desktop/24.10.0/64bit/prtg-desktop-offline-24.10.0-64bit.exe"
+  checksum64     = "9a3b060e1ac1818779ca06bec82343a7d96e8afb"
   checksumType64 = "SHA1"
 
   silentArgs     = '/quiet /norestart'
