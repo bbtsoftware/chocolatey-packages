dce2986672a577b0762194d226358583e5d2f09e
BBT Software AG <devadmin@bbtsoftware.ch>
2025-06-23 10:15:56 +0000

    AU: 1 updated - ssis-vs2022 [skip ci] https://gist.github.com/bbt-bot/b601bc1e0835054f4df23310100983ee/4b34bb08cf43efc99fd63db8e4aa14aebe143635

--- automatic/ssis-vs2022/tools/ChocolateyInstall.ps1
+++ automatic/ssis-vs2022/tools/ChocolateyInstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
     fileType      = 'exe'
     silentArgs    = "/quiet /log `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
     validExitCodes= @(0)
-    url           = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/SSIS/vsextensions/MicrosoftDataToolsIntegrationServices/1.6/vspackage"
-    checksum      = "6c9ba08d967388d5e2e5de73f749e8c6a0dbb4a5a8446e6aaac6f37cd020607b"
+    url           = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/SSIS/vsextensions/MicrosoftDataToolsIntegrationServices/1.6.2/vspackage"
+    checksum      = "08ee9fb20f1f613b6ab3c6248a1dd05d53567c6c43e176430d296bf2d1b98efb"
     checksumType  = "sha256"
   }
 
