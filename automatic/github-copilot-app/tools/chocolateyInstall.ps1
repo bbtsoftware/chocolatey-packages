d903ae232f6bec02d48c02813572aa4def902341
BBT Software AG <devadmin@bbtsoftware.ch>
2026-09-01 16:03:31 +0000

    AU: 1 updated - github-copilot-app [skip ci] https://gist.github.com/bbt-bot/b601bc1e0835054f4df23310100983ee/6cdcf3a58758bf73956a16cc900d327b478a9a05

--- automatic/github-copilot-app/tools/chocolateyInstall.ps1
+++ automatic/github-copilot-app/tools/chocolateyInstall.ps1
@@ -9,12 +9,13 @@ $packageArgs = @{
         0, # success
         3010 # success, restart required
     )
-    url           = 'https://github.com/github/app/releases/download/v1.1.3/GitHub-Copilot-windows-x64.msi'
-    checksum      = '72fc16b4f1196900ba06276c29c29b9ed419dda90d70cd460c1cd7e598be9fdf'
+    url           = 'https://github.com/github/app/releases/download/v1.1.14/GitHub-Copilot-windows-x64.msi'
+    checksum      = '7d685d39f294dda1470c3c2a1af3cbfcb0b5cfb949fdcc9dedc33381cedccc28'
     checksumType  = "sha256"
   }
 
 Install-ChocolateyPackage @packageArgs
+
 
 
 
